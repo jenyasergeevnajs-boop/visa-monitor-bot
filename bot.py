965c5ad13fbdb29878a67ae069563788d7d8ff31
jenyasergeevnajs-boop <jenya.sergeevna.js@gmail.com>
2026-09-25 08:50:18 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import os
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message, "Бот запущен и работает ✅")
+
+bot.infinity_polling()
